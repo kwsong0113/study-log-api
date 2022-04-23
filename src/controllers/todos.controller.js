ca34ec0081901b0d6d6d9d5c7c22b4acdbc4bcf0
Kiwhan Song <kwsong0113@gmail.com>
2022-04-23 13:07:45 +0000

    Implement api for get / posting todos data

---
+++ src/controllers/todos.controller.js
@@ -0,0 +1,26 @@
+const { getTodosDb, postTodosDb } = require('../db');
+
+const getTodos = async (req, res) => {
+  try {
+    const response = await getTodosDb(req.params.username);
+    if (response.noUser) {
+      res.sendStatus(404);
+    } else {
+      res.status(200).json(response);
+    }
+  } catch(err) {
+    res.sendStatus(500);
+  }
+};
+
+const postTodos = async (req, res) => {
+  try {
+    const { _id, notes } = req.body;
+    await postTodos(_id, data);
+    res.sendStatus(200);
+  } catch(err) {
+    res.sendStatus(500);
+  }
+}
+
+module.exports = { getTodos, postTodos };
